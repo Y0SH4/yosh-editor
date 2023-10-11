2935ac4179e216ecdb6796fc3eee0dd9ffbd2547
Y0SH4 <yosa.agung13@gmail.com>
2023-10-12 04:01:38 +0700

    update bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -25,9 +25,15 @@ require("bufferline").setup{
     max_prefix_length = 13,
     tab_size = 18,
     enforce_regular_tabs = false,
-    view = "multiwindow",
     show_buffer_close_icons = true,
-    separator_style = "slant"
+    view = "multiwindow",
+    separator_style = "thick",
+    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+    show_buffer_icons = true,
+    show_close_icon = true,
+    show_tab_indicators = true,
+    persist_buffer_sort = true,
+    always_show_bufferline = true,
   },
 
   highlights = {
